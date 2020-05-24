d7c7cdc2ea178fe2774f9fdc9fa5fbff55caebe8
Miles Au <miles.d.au@gmail.com>
2020-05-24 07:10:34 -0400

    Updates

---
+++ precache-manifest.07456e470682a2ee4bcd6be698f7e879.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb8ab80549fb3d1fa869c03dd73e6e6",
+    "url": "/clothing_ruler_website/index.html"
+  },
+  {
+    "revision": "a7ebeaa50f06687a3344",
+    "url": "/clothing_ruler_website/static/css/main.0d0eb7b5.chunk.css"
+  },
+  {
+    "revision": "15c2d5f034cf12eaf03d",
+    "url": "/clothing_ruler_website/static/js/2.109e6b3d.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/clothing_ruler_website/static/js/2.109e6b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ebeaa50f06687a3344",
+    "url": "/clothing_ruler_website/static/js/main.1d4bda0d.chunk.js"
+  },
+  {
+    "revision": "e0c6f89bceee553f54b6",
+    "url": "/clothing_ruler_website/static/js/runtime-main.f6a588c6.js"
+  },
+  {
+    "revision": "6065204a06334a3d449738f5198b4c6e",
+    "url": "/clothing_ruler_website/static/media/google_badge.6065204a.png"
+  }
+]);
