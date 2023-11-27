7b2318b78abdac7511c371466515f4d9b9fc61c0
Miles Au <miles.d.au@gmail.com>
2023-11-27 20:28:44 +0100

    Update privacy policy

--- src/components/PrivacyPolicy/PrivacyPolicy.js
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -8,7 +8,54 @@ function PrivacyPolicy() {
             </section>
 
             <section>
-                <p>We do not store any personal information or data.</p>
+
+            <p>Last updated: November 27, 2023</p>
+
+            <p>This Privacy Policy explains how Kyle Au ("us," "we," or "our") collects, uses, and discloses information obtained through the "Clothing Ruler" mobile application ("the App") when you use our services.</p>
+
+            </section>
+
+            <section>
+            <p>Information We Collect</p>
+
+            <p>We may collect certain information when you use the App, including but not limited to:</p>
+
+            <ul>
+              <li>Usage Data: We collect information about how you interact with the App, such as the features you use and the time spent on different sections.</li>
+              <li>Device Information: We may collect information about your device, including the device model, operating system version, unique device identifiers, and mobile network information.</li>
+              <li>Firebase Analytics: We use Firebase Analytics, a third-party service provided by Google, to collect and analyze usage data. This may include information about in-app events, errors, and other interactions.</li>
+            </ul>
+
+            <p>We use the collected information for the following purposes:</p>
+
+            <ul>
+              <li>To Improve the App: We analyze usage data to understand how users interact with the App, identify areas for improvement, and enhance the overall user experience.</li>
+              <li>To Provide Support: Collected information may be used to respond to user inquiries, provide technical support, and address issues with the App.</li>
+              <li>Analytics: We use Firebase Analytics to gain insights into user behavior and improve the App's performance and features.</li>
+            </ul>
+
+            <p>We do not sell or share your personally identifiable information with third parties. However, we may share aggregated, non-personally identifiable information for analytical purposes.</p>
+
+            </section>
+
+            <section>
+            <p>Security</p>
+
+            <p>We take reasonable measures to protect the information we collect from unauthorized access, disclosure, alteration, or destruction.</p>
+
+            </section>
+
+            <section>
+            <p>Changes to This Privacy Policy</p>
+
+            <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+
+            </section>
+
+            <section>
+              <p>Contact Us</p>
+
+              <p>If you have any questions about this Privacy Policy or the use of your data, please contact us at miles.d.au@gmail.com.</p>
             </section>
 
         </div>
