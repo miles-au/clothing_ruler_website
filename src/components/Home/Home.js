c0505b494cc8061ebc39b0db2ad8dd3f5b430eb1
Miles Au <miles.d.au@gmail.com>
2023-11-27 20:17:06 +0100

    Update app download icon with link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './Home.scss';
 
 const HomePage = () => (
-    <div className="home-page container">
-        <h1 className="mt0">Download the mobile app</h1>
+    <div className="home-page">
         <section className="mw5 mw7-ns center pa3 ph5-ns" >
             <div className="badge-container">
-                <a href="/" target="_blank" className="store-badge">
+                <a href="https://apps.apple.com/us/app/clothing-ruler-measure-in-ar/id1514959523" target="_blank" className="store-badge">
                     <img className="store-badge-img" src={require('./apple_badge.png')} alt="download on the app store" />
                 </a>
+            </div>
+        </section>
+        <section className="mw5 mw7-ns center pa3 ph5-ns" >
+            <div className="badge-container">
+                
             </div>
         </section>
     </div>
